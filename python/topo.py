367236189d0465091cde9f09d28a01239e105465
Lduf <lucas.dufour@insa-lyon.fr>
2022-01-13 11:39:05 +0100

    change topo.py

--- python/topo.py
+++ python/topo.py
@@ -21,6 +21,7 @@ def write_topology(links_summary, nodes_summary):
 
     for nodes in nodes_summary :
         topo[nodes[0]]["router_id"] = nodes[3]
+        topo[nodes[0]]["parameters"] = {}
 
     for link in links_summary :
         topo[link[0]]["interfaces"] = {}
@@ -30,10 +31,13 @@ def write_topology(links_summary, nodes_summary):
         topo[link[0]]["interfaces"][link[1]] = {}
         topo[link[0]]["interfaces"][link[1]]["neighbor"] = {"name" : link[2], "interface" : link[3]}
         topo[link[0]]["interfaces"][link[1]]["protocols"] = {}
+        topo[link[0]]["interfaces"][link[1]]["parameters"] = {}
         topo[link[0]]["interfaces"][link[1]]["ip"] = {}
+
         topo[link[2]]["interfaces"][link[3]] = {}
         topo[link[2]]["interfaces"][link[3]]["neighbor"] = {"name" : link[0], "interface" : link[1]}
         topo[link[2]]["interfaces"][link[3]]["protocols"] = {}
+        topo[link[2]]["interfaces"][link[3]]["parameters"] = {}
         topo[link[2]]["interfaces"][link[3]]["ip"] = {}
 
     print(topo)
@@ -65,6 +69,3 @@ nodes_summary = lab.nodes_summary(is_print=False)
 print(nodes_summary)
 
 write_topology(links_summary, nodes_summary)
-
-exit(0)
-
